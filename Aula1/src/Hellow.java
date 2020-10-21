575c016733e90a5d4b0f00ee6c2f7585626a069f
Silvio Rodrigues Vieira <srodrvie@everis.com>
2020-10-21 12:57:23 -0300

    comitar aula for while etc..

--- Aula1/src/Hellow.java
+++ Aula1/src/Hellow.java
@@ -6,13 +6,113 @@ public class Hellow {
 	
 		
 		
-		Scanner s = new Scanner(System.in);
+		Scanner s = new Scanner (System.in);
+    
+		int nota;
+    
+       
+      
+     
+//System.out.println("Hellow world");
 		
-		System.out.println("Escreva seu nome");
-		String nome = s.next();
 		
-		System.out.println("hellow world" +" "+ nome);
+		
+		
+		
+
+//	for(int i = 0; i < 5; i++) {
+//			
+//			
+//		
+//		
+//		
+//		
+//System.out.println("Escreva um numero:");
+//
+//
+//
+//int x = s.nextInt();
+//
+//
+//		
+//if ( x  %  2 == 0   ) {
+//	
+//	
+//	System.out.println("PAR" + " "+ x);
+//	
+//	
+//	
+//}
+//
+//else {
+//	
+//	System.out.println("IMPAR" +" "+ x);
+//	
+//}
+//
+//  
+       
+//       int x = 0;
+		
+		
+//--------------------------------------------------
+		
+		
+//while(  x  < 5) {  // Laço para mostrar quantas vez o laço de repetição rodou
+//	
+//
+//	
+//System.out.println(x);
+//	
+//	
+//	x++;
+//	
+//}----------------------------------------------------
+
+       
+//  System.out.println("digite um numero pra descobrir o nome da pessoa que voce ira doar"); 
+//  
+// int doação = s.nextInt();
+// 
+// switch(doação) {
+// 
+// case 1: 
+//	 
+//	 System.out.println("Pedro");
+//	 
+//	 break;
+// 
+//case 2: 
+//	System.out.println("joaquina");
+//	
+//	 break;
+// 
+//	 
+//case 3: 
+//	
+//	System.out.println("fulano");
+//	 
+//	 break;
+//
+// 
+//	 
+//case 4: 
+//	
+//	System.out.println("zequinha");
+//	 
+//	 break;
+//
+// default:
+//	 
+//		System.out.println("digite numero de 1 a 4");
+//		break;
+// 
+// }
+//
+//
+//
+//
 
 	}
+	}
 
-}
